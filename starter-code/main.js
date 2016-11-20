2932b6716af282951e8d812dda587970e167f00d
Richard Sinclair <richardssinclair@gmail.com>
2016-11-20 17:25:03 +0000

    updated js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,32 +1,42 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardsInPlay = [ ];
 
+var createBoard = function () {
+    var gameBoard = document.getElementById('game-board');
 
-if (cardOne == cardThree) {
+  for (var i = 0; i < cards.length; i++) {
+
+      var cardElement = document.createElement('div');
+
+    cardElement.setAttribute('data-card', cards[i]);
+
+        cardElement.className = 'card'
+
+        gameBoard.appendChild(cardElement);
+
+        cardElement.addEventListener('click', isTwoCards)
+}
+}
+createBoard()
+
+function isTwoCards() {
+
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  if (cardsInPlay.length === 2) {
+
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+  }
+}
+function isMatch(cardsInPlay) {
+  console.log(cardsInPlay);
+
+if (cardsInPlay[0] == cardsInPlay[1]) {
   alert('You found a match!');
 } else {
   alert('Sorry, try again.');
 }
-
-var createCards = function () {
-
-  var gameBoard = document.getElementById('game-board');
-
-  for (var i = 0; i < 4; i++ ) {
-    console.log('HELLO'+i);
-
-
-    var newCardItem = document.createElement('div');
-
-    newCardItem.className = 'card'
-
-    gameBoard.appendChild(newCardItem);
-
-  }
 }
-
-createCards()
