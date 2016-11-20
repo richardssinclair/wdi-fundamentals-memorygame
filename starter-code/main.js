0a334521ec7c47c51a907c6db08cc084c5ff9099
Richard Sinclair <richardssinclair@gmail.com>
2016-11-20 16:31:09 +0000

    updatedJs

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,32 @@
 console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+
+if (cardOne == cardThree) {
+  alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+
+var createCards = function () {
+
+  var gameBoard = document.getElementById('game-board');
+
+  for (var i = 0; i < 4; i++ ) {
+    console.log('HELLO'+i);
+
+
+    var newCardItem = document.createElement('div');
+
+    newCardItem.className = 'card'
+
+    gameBoard.appendChild(newCardItem);
+
+  }
+}
+
+createCards()
